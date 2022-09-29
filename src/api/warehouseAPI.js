8c7108674ca2e37da372c590162d0e4d0a9612a7
nhttoan0809 <111209609+nhttoan0809@users.noreply.github.com>
2022-09-29 08:49:44 +0700

    Change method from GET to POST for request warehouse import

--- src/api/warehouseAPI.js
+++ src/api/warehouseAPI.js
@@ -6,7 +6,7 @@ const warehouseAPI = {
     // [GET] agent/:id_agent/warehouse/get_all
     // [GET] agent/:id_agent/warehouse/add
     // [POST] agent/:id_agent/warehouse/:id_warehouse/update
-    // [GET] agent/:id_agent/warehouse/:id_warehouse/import
+    // [POST] agent/:id_agent/warehouse/:id_warehouse/import
     // [DELETE] agent/:id_agent/warehouse/:id_warehouse/export
     // [DELETE] agent/:id_agent/warehouse/:id_warehouse/delete
 
